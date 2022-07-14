2eda872fc24f4568a5098c13b5a4acbb86426af3
Daniel <jdvela158@gmail.com>
2022-07-13 19:06:48 -0600

    Finish all operations

--- src/main/java/com/mycompany/mavenproject1/FrameCalc.java
+++ src/main/java/com/mycompany/mavenproject1/FrameCalc.java
@@ -245,15 +245,30 @@ public class FrameCalc extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        // Add:
+        int a = Integer.parseInt(n1.getText());
+        int b = Integer.parseInt(n2.getText());
+        
+        String c = String.valueOf(a / b);
+        res.setText(c);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        // Add:
+        int a = Integer.parseInt(n1.getText());
+        int b = Integer.parseInt(n2.getText());
+        
+        String c = String.valueOf(a - b);
+        res.setText(c);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
+        // Add:
+        int a = Integer.parseInt(n1.getText());
+        int b = Integer.parseInt(n2.getText());
+        
+        String c = String.valueOf(a * b);
+        res.setText(c);
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void resActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_resActionPerformed
